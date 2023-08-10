8616bf709cf65550d3abc6162dab10e4e9114b88
Alex Ryabich <alexryabich@mail.ru>
2023-08-10 20:56:49 +0300

    тест8.1

--- controllers/cards.js
+++ controllers/cards.js
@@ -31,19 +31,15 @@ async function createCard(req, res) {
 async function deleteCard(req, res) {
   try {
     const { cardId } = req.params;
-    const card = await Card.findById(cardId);
+    const card = await Card.findByIdAndDelete(cardId);
 
     if (!card) {
-      const error = new Error('Карточка не найдена');
-      error.name = 'NotFoundError';
-      throw error;
+      res.status(404).send({ message: 'Карточка не найдена' });
     }
 
     res.send(card);
   } catch (err) {
-    if (err.message === 'Not found') {
-      res.status(404).send({ message: 'Карточка не найдена' });
-    } else if (err.name === 'CastError') {
+    if (err.name === 'CastError') {
       res.status(400).send({ message: 'Переданы некорректные данные о карточке' });
     } else {
     res.status(500).send({ message: err.message });
@@ -76,8 +72,12 @@ async function addLike(req, res) {
 
 async function deleteLike(req, res) {
   try {
-    const { cardId } = req.params;
-    const card = await Card.findByIdAndDelete(cardId);
+    const userId = req.user._id;
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: userId } },
+      { new: true },
+    );
 
     if (!card) {
       res.status(404).send({ message: 'Карточка не найдена' });
